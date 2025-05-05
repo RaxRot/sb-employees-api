ff7330e74d945c4049a84ad3332603f145b53104
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 17:07:20 +0100

    add EmployeeServiceImpl with createMethod

---
+++ src/main/java/com/raxrot/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.raxrot.ems.service.impl;
+
+import com.raxrot.ems.dto.EmployeeDTO;
+import com.raxrot.ems.entity.Employee;
+import com.raxrot.ems.repository.EmployeeRepository;
+import com.raxrot.ems.service.EmployeeService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
+        this.employeeRepository = employeeRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public EmployeeDTO createEmployee(EmployeeDTO employeeDTO) {
+        Employee employee = modelMapper.map(employeeDTO, Employee.class);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return modelMapper.map(savedEmployee, EmployeeDTO.class);
+    }
+}
