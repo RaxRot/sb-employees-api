b0c44c8294c6a1ba9f5d384b03137ffea57ce299
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 17:11:56 +0100

    add EmployeeController with create

---
+++ src/main/java/com/raxrot/ems/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.raxrot.ems.controller;
+
+import com.raxrot.ems.dto.EmployeeDTO;
+import com.raxrot.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO savedEmployee=employeeService.createEmployee(employeeDTO);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
