19f93c917a7f0891adf11503c92db0773561a1b4
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 17:40:18 +0100

    add logs

--- src/main/java/com/raxrot/ems/controller/EmployeeController.java
+++ src/main/java/com/raxrot/ems/controller/EmployeeController.java
@@ -2,12 +2,14 @@ package com.raxrot.ems.controller;
 
 import com.raxrot.ems.dto.EmployeeDTO;
 import com.raxrot.ems.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
@@ -18,30 +20,35 @@ public class EmployeeController {
 
     @PostMapping
     public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        EmployeeDTO savedEmployee=employeeService.createEmployee(employeeDTO);
+        log.info("Creating employee with email: {}", employeeDTO.getEmail());
+        EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long id) {
-        EmployeeDTO savedEmployee=employeeService.getEmployeeById(id);
+        log.info("Fetching employee with id: {}", id);
+        EmployeeDTO savedEmployee = employeeService.getEmployeeById(id);
         return new ResponseEntity<>(savedEmployee, HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<List<EmployeeDTO>> getEmployees() {
-        List<EmployeeDTO>employeeDTOS=employeeService.getAllEmployees();
+        log.info("Fetching all employees");
+        List<EmployeeDTO> employeeDTOS = employeeService.getAllEmployees();
         return new ResponseEntity<>(employeeDTOS, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO) {
-        EmployeeDTO savedEmployee=employeeService.updateEmployee(id, employeeDTO);
+        log.info("Updating employee with id: {}", id);
+        EmployeeDTO savedEmployee = employeeService.updateEmployee(id, employeeDTO);
         return new ResponseEntity<>(savedEmployee, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<EmployeeDTO> deleteEmployee(@PathVariable Long id) {
+        log.warn("Deleting employee with id: {}", id);
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
     }
