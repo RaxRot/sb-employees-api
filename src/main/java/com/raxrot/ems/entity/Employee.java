57427ed1f345a4472a2dbeb78615e0ef726013d6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 16:58:46 +0100

    add Employee model

---
+++ src/main/java/com/raxrot/ems/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.raxrot.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id",nullable = false, unique = true)
+    private String email;
+}
