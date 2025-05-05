6e12ef0cec0f2a24220a460689964b05bff48732
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 17:45:51 +0100

    add Swagger Definition

--- src/main/java/com/raxrot/ems/SbEmployeesApiApplication.java
+++ src/main/java/com/raxrot/ems/SbEmployeesApiApplication.java
@@ -1,7 +1,23 @@
 package com.raxrot.ems;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee Management API",
+                version = "1.0",
+                description = "API for managing employees in the system",
+                contact = @Contact(
+                        name = "RaxRot",
+                        email = "dasistperfektos@email.com",
+                        url = "https://github.com/RaxRot"
+                )
+        )
+)
 
 @SpringBootApplication
 public class SbEmployeesApiApplication {
