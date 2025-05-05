daf2244e8df8eec8457d6c7997ddcede241d3d89
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-05 16:22:50 +0100

    init

---
+++ src/main/java/com/raxrot/ems/SbEmployeesApiApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEmployeesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbEmployeesApiApplication.class, args);
+    }
+
+}
